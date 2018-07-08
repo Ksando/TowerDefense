621a75cfef65647df2db7abdac809d4efcf89178
absdspr <kiriys36@gmail.com>
2018-07-08 17:24:58 +0300

    Add some new scripts
    some changes

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+
+    private string className;
+    private int money;
+    Abillity abillity = new Abillity();
+    public int getMoney()
+    {
+        return money;
+    }
+    //Adding money to player
+    public void setMoney(int value)
+    {
+       
+       money = money + value;
+    }
+    public string getClassName()
+    {
+        return className;
+    }
+    public void setClassName(string value)
+    {
+        className = value;
+    }
+    public bool buySomething(int value)
+    {
+        if (this.money >= value)
+        {
+            this.setMoney(-value);
+            return true;
+        }
+        Debug.Log("no money");
+        return false;
+    }
+   
+	// Use this for initialization
+	void Start () {
+        money = 150;
+        abillity.setAbilltys(className);
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
