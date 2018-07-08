642725e4c0bfd81eda7fc2eff3cce84f441a607d
folyyy <folyhideout@gmail.com>
2018-07-08 13:42:53 +0300

    Саша
    Добавил поле с клетками-объектами, чтобы Андрюха начал работать с башнями(У поля "TowerPlacement" есть тэг. Используя его Андрей должен написать скрипт для построения башен только в этих местах). Добавил хп у противников и потестил на башне, которая при столкновении наносит урон. Если сделать урон максимальный, то противник умирает(удаляется) .Сделал противникам путь до базы.

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Enemy : MonoBehaviour
+{
+
+    public float health = 100;
+    public int reward = 10;
+    public Image healthBar;
+    public float maxHealth = 100f;
+    private void Start()
+    {
+
+    }
+
+  //  public void TakeDamage(float amount)
+  //  {
+  //      health -= amount;
+  //      if (health <= 0)
+  //          Die();
+  //  }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "tower")
+         {
+        Debug.Log("TAGGG");
+        if (health > 0)
+        {
+            //decrease enemy health
+            health -= 10;
+                healthBar.fillAmount = health / maxHealth;
+            if (health <= 0)
+            {
+                Die();
+            }
+        }
+          }
+    }
+
+    private void Die()
+    {
+        Destroy(gameObject);
+    }
+}
