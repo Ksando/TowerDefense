5107dc527699abe2fa294b54456f4eda4a78a97a
absdspr <kiriys36@gmail.com>
2018-07-14 11:10:29 +0300

    pause menu

--- Assets/Scripts/PauseScript.cs
+++ Assets/Scripts/PauseScript.cs
@@ -1,16 +1,46 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PauseScript : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    public GameObject pauseButton;
+    public GameObject pauseMenu;
+    public GameObject settinsMenu;
+
+    void Start()
+    {
+       pauseButton.SetActive(true);
+       pauseMenu.SetActive(false);
+       settinsMenu.SetActive(false);
+
+       Time.timeScale = 1f;
+    }
+    public void PauseGame()
+    {
+        pauseButton.SetActive(false);
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+    }
+    public void resumeGame()
+    {
+        pauseButton.SetActive(true);
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+    }
+    public void menu()
+    {
+        SceneManager.LoadScene("FirstScene");
+    }
+    public void gameSettings()
+    {
+        pauseMenu.SetActive(false);
+        settinsMenu.SetActive(true);
+        
+        
+
+    }
+
 }
